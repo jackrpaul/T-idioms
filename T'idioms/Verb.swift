1cf1f5413c58c065ffac9515c079f85082e982f6
jackrpaul <jackrpaul13@gmail.com>
2022-03-10 12:51:45 -0500

    Added Verb class to keep track of verb list

---
+++ T'idioms/Verb.swift
@@ -0,0 +1,39 @@
+//
+//  Verb.swift
+//  T'idioms
+//
+//  Created by Jack Paul on 3/10/22.
+//
+
+import UIKit
+
+class Verb {
+    struct Verbs {
+        let verbList = [
+            "acceleratin'",
+            "achin'",
+            "actin'",
+            "advancin'",
+            "agin'",
+            "aimin'",
+            "answerin'",
+            "apologizin'",
+            "arguin'",
+            "arrivin'",
+            "autographin'",
+            "babblin'",
+            "backin'",
+            "bailin'",
+            "bargainin'",
+            "barterin'",
+            "battin'",
+            "bathin'",
+            "bawlin'",
+            "battlin'",
+            "bein'",
+            "beggin'",
+            "behavin'",
+            "",
+        ]
+    }
+}
