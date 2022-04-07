b652f8d3db9e3a435a7d682e2e404daf6a2cfbf4
pauljr2 <pauljr2@miamioh.edu>
2022-04-06 20:58:26 -0400

    add entries for verbs and comparative adjs

--- T'idioms/Adj.swift
+++ T'idioms/Adj.swift
@@ -9,6 +9,37 @@ import Foundation
 
 class Adj {
     func generateAdjs() -> [String] {
-        return ["speedier"]
+        return [
+            "bolder",
+            "brighter",
+            "calmer",
+            "duller",
+            "faster",
+            "grosser",
+            "higher",
+            "meaner",
+            "neater",
+            "odder",
+            "plainer",
+            "weaker",
+            "younger",
+            "bigger",
+            "larger",
+            "dimmer",
+            "sadder",
+            "speedier",
+            "clumsier",
+            "fancier",
+            "guiltier",
+            "happier",
+            "lonelier",
+            "nastier",
+            "prettier",
+            "riskier",
+            "sillier",
+            "worse",
+            "further",
+            "better",
+        ]
     }
 }
